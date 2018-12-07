fafaaa2b40740f6bd8d74f97482a9aad46f4741f
Evgene <evisotskiy@zaraffasoft.com>
2018-12-07 15:26:26 +0200

    Cleaning

--- src/index.jsx
+++ src/index.jsx
@@ -20,29 +20,3 @@ if (module.hot) {
         });
     }
 })(jQuery);
-
-// for testing during the developing with working hot module replacement. Not passing to the build
-if (process.env.NODE_ENV === 'development') {
-    const searchFieldConfig__singleSelectBox = require('./stories/jsonExamples/searchField--singleSelectBox');
-    const searchFieldConfig__multiSelectBox  = require('./stories/jsonExamples/searchField--multiSelectBox');
-    const searchFieldConfig__googlePlaces    = require('./stories/jsonExamples/searchField--googlePlaces');
-
-    const datesFieldsConfig = require('./stories/jsonExamples/datesFields');
-
-    const guestsFieldConfig__singleSelectBox = require('./stories/jsonExamples/guestsField--singleSelectBox');
-
-
-    $(function () {
-        $('#root').rentivoSearchbar({
-            baseUrl:      'http://localhost:3000/',
-            appendString: "?aff=123",
-            searchField:  { ...searchFieldConfig__singleSelectBox },
-            datesFields:  { ...datesFieldsConfig },
-            guestsField:  { ...guestsFieldConfig__singleSelectBox }
-        });
-    });
-
-}
-
-// todo: check PropTypes of deepest components
-// todo: remove PropTypes from routing components
